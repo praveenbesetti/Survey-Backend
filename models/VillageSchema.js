297e2f65cf56eeeaf3d089583cc9dae38c7109f8
praveenbesetti <praveenbesetti0@gmail.com>
2026-03-14 10:29:03 +0530

    new fix

--- models/VillageSchema.js
+++ models/VillageSchema.js
@@ -6,7 +6,7 @@ const villageSchema = new mongoose.Schema({
     username: { type: String, required: true },
     password: { type: String, required: true },
     token:{ type: String },
-    SurveyId: { type: String },
+    surveyId: { type: String },
     isAuthorized:{type:Boolean, default:true},
     name: { type: String, required: true },
     phone: { type: String },
